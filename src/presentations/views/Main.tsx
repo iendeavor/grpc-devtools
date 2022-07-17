8c4cdd5cdba3e0a1014559105277dd50a7a525b2
Ernest <iendeavor.github.io@gmail.com>
2022-07-17 12:54:44 +0800

    feat: should not open detail on init
    same behavior with the Network panel

--- src/presentations/views/Main.tsx
+++ src/presentations/views/Main.tsx
@@ -1,24 +1,12 @@
 import React, { useEffect, useMemo } from "react";
 import RequestDetail from "./main/RequestDetail";
 import RequestRows from "./main/RequestRows";
-import { RequestRow } from "@/entities/request-row";
 import useDetail from "@/presentations/composables/use-detail";
 import useRequestRows from "@/presentations/composables/use-request-rows";
 
 const Main = ({ headerHeight }: { headerHeight: number }) => {
   const [detail, setDetail] = useDetail();
   const [requestRows] = useRequestRows();
-  useEffect(() => {
-    setFirstRequestRowAsDetailIfMissing(requestRows);
-  }, [requestRows]);
-  const setFirstRequestRowAsDetailIfMissing = (requestRows: RequestRow[]) => {
-    if (requestRows.length === 0) return;
-    if (detail.requestId !== null) return;
-    setDetail({
-      ...detail,
-      requestId: requestRows[0]!.id,
-    });
-  };
   useEffect(() => {
     resetDetailRequestIdIfRequestRowsIsEmpty();
   }, [requestRows]);
