096b2e17c9ce10ca2b22e60a5e596bc194892515
Ernest <iendeavor.github.io@gmail.com>
2022-07-17 18:14:31 +0800

    fix: close detail when filter changes

--- src/presentations/views/Header.tsx
+++ src/presentations/views/Header.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HorizontalDivider from "@/presentations/components/HorizontalDivider";
 import FilterBar from "./header/FilterBar";
 import Toolbar from "./header/Toolbar";
 import useConfig from "@/presentations/composables/use-config";
+import useFilter from "@/presentations/composables/use-filter";
+import useDetail from "@/presentations/composables/use-detail";
 
 const Header = ({
   headerRef,
@@ -10,6 +12,15 @@ const Header = ({
   headerRef: React.ClassAttributes<HTMLElement>["ref"] | null;
 }) => {
   const [config] = useConfig();
+
+  const [filter] = useFilter();
+  const [detail, setDetail] = useDetail();
+  useEffect(() => {
+    setDetail({
+      ...detail,
+      requestId: null,
+    });
+  }, [filter]);
 
   return (
     <header ref={headerRef} className="flex flex-col bg-background-elevation-1">
